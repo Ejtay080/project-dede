52f86a326d0a07081aa04ca96280341da4c6a1c3
Evan <Kinsler>
2020-08-14 16:00:50 -0700

    Added more images and made first HTML
    Added several more images and started the index page.

---
+++ js/test.js
@@ -0,0 +1,15 @@
+function indexPage() {
+    location.replace("index.html");
+}
+function careerPage() {
+    location.replace("career.html");
+}
+function galleryPage() {
+    location.replace("gallery.html");
+}
+function aboutMePage() {
+    location.replace("aboutme.html");
+}
+function resumePage() {
+    location.replace("resume.html");
+}
